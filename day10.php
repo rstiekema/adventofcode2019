9a811941ff6522d2fddf9a422a35b5b69033639a
Rutger Stiekema <rutger@stiekema.eu>
2019-12-13 00:36:34 +0100

    Finished day 10

--- day10.php
+++ day10.php
@@ -74,11 +74,24 @@ $input = file_get_contents('resources/day10.txt');
 
 
 
-$map     = new AsteroidMap($input);
-$asteroid = $map->getBestLocationAsteroid();
+$map  = new AsteroidMap($input);
 
+
+$asteroid = $map->getBestLocationAsteroid();
 echo "Best location asteroid: {$asteroid->getCoordinates()} (".count($map->getVisibleAsteroidsFrom($asteroid))." visible asteroids from this point)\n";
 
+$asteroids = $map->getAsteroids();
+echo "Total asteroids: ".count($map->getAsteroids())."\n";
+
+$vaporizeOrder = $map->getVaporizeOrder($asteroid);
+$vaporize200th = $vaporizeOrder[199];
+echo "200th asteroid of ".count($vaporizeOrder)." to be vaporized: ".$vaporizeOrder[199]."\n";
+echo "Part 2 result: ".($vaporize200th->getX() * 100 + $vaporize200th->getY())."\n";
+
+//echo "Vaporize order:\n";
+//foreach ($vaporizeOrder as $key => $vaporizeAsteroid) {
+//	echo ($key + 1).": ".str_pad($vaporizeAsteroid->getCoordinates(), 9)." (".$vaporizeAsteroid->getAngleFrom($asteroid->getX(), $asteroid->getY())."deg)\n";
+//}
 
 
 
@@ -121,29 +134,58 @@ class AsteroidMap
 	}
 	
 	
-	
-	public function getVisibleAsteroidsFrom(Asteroid $asteroid): array
+	public function getVisibleAsteroidsFrom(Asteroid $asteroid, array $asteroids = null): array
 	{
 		$x = $asteroid->getX();
 		$y = $asteroid->getY();
 		
-		$asteroids = [];
+		if ($asteroids === null) {
+			$asteroids = $this->getAsteroids();
+		}
 		
-		foreach ($this->sortAsteroidsByDistance($this->asteroids, $x, $y) as $targetAsteroid) {
+		$visible = [];
+		
+		foreach ($this->sortAsteroidsByDistance($asteroids, $x, $y) as $targetAsteroid) {
 			$angle = $targetAsteroid->getAngleFrom($x, $y);
 			
 			if ($targetAsteroid->getX() == $x && $targetAsteroid->getY() == $y) {
 				continue;
 			}
 			
-			if (!isset($asteroids[(string)$angle])) {
-				$asteroids[(string)$angle] = $targetAsteroid;
+			if (!isset($visible[(string)$angle])) {
+				$visible[(string)$angle] = $targetAsteroid;
 			}
 		}
 		
-		return $asteroids;
+		return array_values($visible);
 	}
 	
+	
+	public function getVaporizeOrder(Asteroid $from): array
+	{
+		$sourceX   = $from->getX();
+		$sourceY   = $from->getY();
+		$asteroids = $this->getAsteroids();
+		
+		// Let's not vaporize our source asteroid...
+		foreach ($asteroids as $asteroidKey => $asteroid) {
+			if ($asteroid->getX() == $sourceX && $asteroid->getY() == $sourceY) {
+				unset($asteroids[$asteroidKey]);
+				break;
+			}
+		}
+		
+		$vaporizeOrder = [];
+		
+		while (!empty($asteroids)) {
+			$visible       = $this->getVisibleAsteroidsFrom($from, $asteroids);
+			$visible       = $this->sortAsteroidsByAngle($visible, $sourceX, $sourceY);
+			$vaporizeOrder = array_merge($vaporizeOrder, $visible);
+			$asteroids     = array_diff($asteroids, $visible);
+		}
+		
+		return $vaporizeOrder;
+	}
 	
 	
 	private function sortAsteroidsByDistance(array $asteroids, int $fromX, int $fromY): array
@@ -158,6 +200,24 @@ class AsteroidMap
 			
 			if ($distanceA == $distanceB) return 0;
 			return $distanceA < $distanceB ? -1 : 1;
+		});
+		
+		return $asteroids;
+	}
+	
+	
+	private function sortAsteroidsByAngle(array $asteroids, int $fromX, int $fromY): array
+	{
+		uasort($asteroids, function($a, $b) use ($fromX, $fromY) {
+			/**
+			 * @var Asteroid $a
+			 * @var Asteroid $b
+			 */
+			$angleA = $a->getAngleFrom($fromX, $fromY);
+			$angleB = $b->getAngleFrom($fromX, $fromY);
+			
+			if ($angleA == $angleB) return 0;
+			return $angleA < $angleB ? -1 : 1;
 		});
 		
 		return $asteroids;
@@ -210,7 +270,19 @@ class Asteroid
 	
 	public function getAngleFrom(int $x, int $y): float
 	{
-		return atan2($y - $this->y, $x - $this->x) * 180 / pi();
+		$angle = atan2($y - $this->y, $x - $this->x) * 180 / pi();
+		
+		// 'Up' should be 0 degrees...
+		$angle -= 90;
+		if ($angle < 0) $angle += 360;
+		
+		return $angle;
+	}
+	
+	
+	public function __toString()
+	{
+		return $this->getCoordinates();
 	}
 	
 }
